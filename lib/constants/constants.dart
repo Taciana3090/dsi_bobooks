78083052e30390984169acdcd6b66eca1018eca0
Taciana3090 <taciana3090@gmail.com>
2022-09-24 21:50:17 -0300

    upgrade

--- lib/constants/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Color.fromARGB(255, 214, 37, 184);
-const textColor = Color(0xFF35364F);
-const backgroundColor = Color(0xFFE6EFF9);
-const redColor = Color(0xFFE85050);
-
-const defaultPadding = 16.0;
